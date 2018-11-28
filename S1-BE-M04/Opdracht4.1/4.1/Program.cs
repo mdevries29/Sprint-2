6fa98afd397fbc19bd70dfa9514b6029d2cc8b92
mdevries29 <mdevries15@student.rocvantwente.nl>
2018-11-28 11:00:34 +0100

    4.1,4.6,4,11

---
+++ S1-BE-M04/Opdracht4.1/4.1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int getal1, getal2;
+
+            Console.WriteLine("voer getal1 in: ");
+            getal1 = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("voer getal2 in: ");
+            getal2 = Convert.ToInt16(Console.ReadLine());
+
+            if (getal1 < getal2)
+            {
+                Console.WriteLine("Het tweede getal is het grootste");
+                Console.ReadLine();
+            }
+            else if (getal1 > getal2)
+            {
+                Console.WriteLine("Het eerste getal is het grootste");
+                Console.ReadLine();
+            }
+            else if (getal1 == getal2)
+            {
+                Console.WriteLine("De getallen zijn gelijk aan elkaar");
+                Console.ReadLine();
+            }
+            else
+            Console.WriteLine("geen geldige waarde");
+            Console.ReadLine
+
+        }
+    }
+}
---
+++ S1-BE-M04/Opdracht4.6/4.6/4.6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _4._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string uren, jaren, storing;
+
+            Console.WriteLine("heeft de draaibank meer dan 10.000 uur gewerkt?: ");
+            uren = (Console.ReadLine());
+
+            Console.WriteLine("is de draaibank al 7 jaar of meer oud?: ");
+            jaren = (Console.ReadLine());
+
+            Console.WriteLine("heeft de draaibank meer dan 25 storingen per jaar?: ");
+            storing = (Console.ReadLine());
+
+            if ((uren == "ja") || (jaren == "ja") || (storing == "ja"))
+            {
+                Console.WriteLine("de draaibank moet vervangen worden: ");
+                storing = (Console.ReadLine());
+            }
+            else
+            Console.WriteLine("nee uw draaibank is nog goed en hoeft niet vervangen worden");
+            Console.ReadLine();
+        }
+    }
+}
