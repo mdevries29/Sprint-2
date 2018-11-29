723cbe0e74c2403fa0d163dc506e17acc5d6b81b
mdevries29 <mdevries15@student.rocvantwente.nl>
2018-11-29 11:43:41 +0100

    S1-BE-M03

---
+++ S1-BE-M03/Opdracht3.9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int jaar, maand;
+
+            Console.WriteLine("Geef uw Jaartal");
+            jaar = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Geef uw Maand");
+            maand = Convert.ToInt16(Console.ReadLine());
+
+            int dagenInMaand = System.DateTime.DaysInMonth(jaar, maand);
+            Console.WriteLine("Het aantal dagen in deze maand is " + dagenInMaand.ToString());
+            Console.ReadLine();
+        }
+    }
+}
